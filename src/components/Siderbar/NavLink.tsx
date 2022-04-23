71842770dd943ba6881de7dd7deb739acd0390c6
lucasfouraux <lucasfouraux@gmail.com>
2022-04-23 10:44:04 -0300

    feat: flag active link

--- src/components/Siderbar/NavLink.tsx
+++ src/components/Siderbar/NavLink.tsx
@@ -2,6 +2,7 @@ import { Icon, Link as ChakraLink, LinkProps, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import { ElementType, ReactNode } from "react";
 import { RiInputMethodLine } from "react-icons/ri";
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps extends LinkProps{
   icon: ElementType
@@ -11,11 +12,11 @@ interface NavLinkProps extends LinkProps{
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return(
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" alignItems="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">{children}</Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   )
 }
